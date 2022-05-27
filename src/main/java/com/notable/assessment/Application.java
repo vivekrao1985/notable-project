c8d0bcdbfc06b98d6f1030cb1fc9225a646b5bc4
vivekrao1985 <vivekrao1985>
2022-05-27 23:01:09 +0000

    Initial commit

---
+++ src/main/java/com/notable/assessment/Application.java
@@ -0,0 +1,9 @@
+package com.notable.assessment;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
